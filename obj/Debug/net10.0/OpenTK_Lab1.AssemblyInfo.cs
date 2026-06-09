036eb37f821f5bdd9a4b25738317122374f8edb4
teliryon <egor.boych@yandex.com>
2026-06-10 00:37:47 +0300

    сделал правки

--- obj/Debug/net10.0/OpenTK_Lab1.AssemblyInfo.cs
+++ obj/Debug/net10.0/OpenTK_Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OpenTK_Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f7d578686fbf18beabda524a5658589e16a445")]
 [assembly: System.Reflection.AssemblyProductAttribute("OpenTK_Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OpenTK_Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
